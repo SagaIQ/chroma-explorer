0930f3eaa2af5d9a6703961f54f5d05ec5b0baf8
george-sagaiq <george@sagaiq.ai>
2024-09-16 06:35:40 -0400

    Update search results page

--- src/renderer/src/pages/CollectionSearchResultsPage.tsx
+++ src/renderer/src/pages/CollectionSearchResultsPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useParams } from 'react-router-dom';
-import { Document } from "../../../shared/chroma-service";
+import { Document, DocumentChunk } from "../../../shared/chroma-service";
 import { Channels } from "../../../shared/contants";
 
 type CollectionSearchResultsPageProps = {
@@ -28,7 +28,44 @@ export const CollectionSearchResultsPage: React.FC<CollectionSearchResultsPagePr
   return (
     <>
       {
-        JSON.stringify(documents, null, 2)
+        documents.length === 0 ? <div>No search results for <b>'{searchString}'</b> in collection <b>'{collectionName}'</b></div> : 
+        <>
+          {
+            documents.map(document => (
+              <div className="gridContainer" style={{ marginBottom: '20px'}}>
+                <p style={{ fontSize: '24px', color: 'black'}}>{document.name}</p>
+                <p>{document.path}</p>
+                <table>
+                  <tr>
+                    <th style={{ textAlign: 'left', width: '340px', minWidth: '340px', maxWidth: '340px'}}>Chunk ID</th>
+                    <th style={{ textAlign: 'left' }}>Metadata</th>
+                  </tr>
+                  {
+                  document.chunks.map((chunk: DocumentChunk) => {
+                    return (
+                      <>
+                        <tr>
+                          <td style={{ textAlign: 'left', width: '340px', minWidth: '340px', maxWidth: '340px', color: 'black'}}>{chunk.id}</td>
+                          <td style={{ textAlign: 'left', color: 'black' }}>{JSON.stringify(chunk.metadata)}</td>
+                        </tr>
+                        <tr>
+                          <td colSpan={2}>
+                            <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
+                              <div style={{color: '#606266'}}>
+                                {chunk.content}
+                              </div>
+                              <div className="rowDivider" />
+                            </div>
+                          </td>
+                        </tr>
+                      </>
+                    )
+                  })}
+                </table>
+              </div>
+            ))
+          }
+        </>
       }
     </>
   );
